25ed447cf5b68845a96ce95c122b3a75438c2839
Ron391 <75159085+Ron391@users.noreply.github.com>
2022-07-15 00:46:51 +0530

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+
+namespace Dash11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = new ChartValues<ObservableValue>
+                    {
+                        new ObservableValue(3),
+                        new ObservableValue(5),
+                        new ObservableValue(2),
+                        new ObservableValue(7),
+                        new ObservableValue(7),
+                        new ObservableValue(4)
+                    },
+                    PointGeometrySize = 0,
+                    StrokeThickness = 4,
+                    Fill = Brushes.Transparent
+                },
+                new LineSeries
+                {
+                    Values = new ChartValues<ObservableValue>
+                    {
+                        new ObservableValue(3),
+                        new ObservableValue(4),
+                        new ObservableValue(6),
+                        new ObservableValue(8),
+                        new ObservableValue(7),
+                        new ObservableValue(5)
+                    },
+                    PointGeometrySize = 0,
+                    StrokeThickness = 4,
+                    Fill = Brushes.Transparent
+                }
+            };
+
+            DataContext = this;
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+    }
+}
